3c6352c3e9fbf6ba2fd8b194468a316b86ce4484
Sharkov Ivan <sharkov.agent@mail.ru>
2023-06-17 12:49:19 +0300

    Set unique and validate user email

--- src/auction/models/user.py
+++ src/auction/models/user.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 
 class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(required=True, validators=[
+        RegexValidator(
+            regex=r"^\S+@\S+\.\S+$",
+            message='Неверный формат почты'
+        )
+    ])
+        
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("Данный email уже используется")
+        return value
--- src/auction/services/authService.py
+++ src/auction/services/authService.py
@@ -1,40 +1,37 @@
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.models import User
-from rest_framework import status
-from rest_framework.response import Response
+from auction.models.user import UserSerializer
+from rest_framework import serializers
 
 class AuthService:
     def register(self, username, password, email):
         try:
-            user = User.objects.create_user(username=username, password=password, email=email)
+            serializer = UserSerializer(data={'username': username, 'password': password, 'email': email})
+            serializer.is_valid(raise_exception=True)
+            user = serializer.save()
+
             serialized_user = self.serializeUser(user)
             return {'message': 'Пользователь успешно зарегистрирован', 'data': serialized_user}
+        except serializers.ValidationError as e:
+            return {'message': e.detail}
         except Exception as e:
-            return Response({'message': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return {'message': str(e)}
 
     def login(self, request, username, password):
         try:
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                serialized_user = self.serializeUser(user)
+                serialized_user = UserSerializer(user).data
                 return {'message': 'Пользователь успешно авторизован', 'data': serialized_user}
             else:
                 raise Exception('Неправильный логин или пароль')
         except Exception as e:
-            return Response({'message': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return {'message': str(e)}
 
     def logout(self, request):
         try:
             logout(request)
             return {'message': 'Успешный выход из аккаунта'}
         except Exception as e:
-            return Response({'message': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return {'message': str(e)}
 
-    def serializeUser(self, user):
-        serialized_user = {
-            'username': user.username,
-            'email': user.email,
-            'password': user.password,
-        }
-        return serialized_user
