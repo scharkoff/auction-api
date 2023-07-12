1f7c34103269263a0ffd9bd0d35ba5dea066fe8e
Sharkov Ivan <sharkov.agent@mail.ru>
2023-07-12 17:32:39 +0300

    Add localhost settings in settings.py

--- src/auction/settings.py
+++ src/auction/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')    
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG_VALUE')  
 
-ALLOWED_HOSTS = ['auction-api-5cvf.onrender.com']
+ALLOWED_HOSTS = ['auction-api-5cvf.onrender.com', 'localhost']
 
 # Application definition
 
